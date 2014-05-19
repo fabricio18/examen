e4a86ca61502d7902d93db280ff4fa0711e474cb
fabricio18 <ivethsabillon11@gmail.com>
2014-05-19 08:37:31 -0600

    Update ejemplo2.java

--- src/examen/ejemplo2.java
+++ src/examen/ejemplo2.java
@@ -5,7 +5,7 @@ public class ejemplo2 {
 	
 	public static void main(String args[]){
 		// los codigos usados fueron empleados en clase y se uso el int para introducir las variables empleadas
-
+//Informacion obtenida:https://github.com/IvethS/Examen
 		Scanner lea = new Scanner(System.in);
 		System.out.print("Ingrese un numero ");
 		
